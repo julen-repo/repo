45fea6361806bf6b4e644ab8a3c8778fad35e067
julen-repo <julen.hernandez.omanas@iesjulianmarias.es>
2024-10-17 12:33:36 +0200

    fdfasd

--- funcion3.php
+++ funcion3.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        th, td {
+            border: 1px solid black;
+            text-align: center;
+            padding: 5px;
+        }
+    </style>
 </head>
 
 <body>
