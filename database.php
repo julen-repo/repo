4f8d728c6ed2b15f8d0abff469abb71da137757e
julen-repo <julen.hernandez.omanas@iesjulianmarias.es>
2024-10-18 12:49:17 +0200

    a

--- database.php
+++ database.php
@@ -16,6 +16,15 @@
 
     $conn = new mysqli($servername, $username, $password, $database);
 
+    $sql="SELECT id_curso,curso,plazas_totales,plazas_ocupadas FROM cursos";
+    $result=$conn->query($sql);
+    if($result){
+        while($row = $result->fetch_assoc()){
+            echo "ID: ".$row["id_curso"]." Curso: ".$row["curso"]." Plazas totales: ".$row["plazas_totales"]." Plazas ocupadas: ".$row["plazas_ocupadas"]."<br>";
+        }
+        $result->free();
+    }
+
     if ($conn->connect_error) {
         die("Conexion fallida: " . $conn->connect_error);
     } else {
